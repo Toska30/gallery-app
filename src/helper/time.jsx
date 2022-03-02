2cf1ebab13e671eeee733926219cc57e3c6ec43d
Blerta Toska <36380101+Toska30@users.noreply.github.com>
2022-03-02 09:57:25 +0100

    Delete time.jsx

--- src/helper/time.jsx
+++
@@ -1,6 +0,0 @@
-export const firebaseTimestampToString = (fbTs) => {
-	if (!fbTs) return null;
-
-	const dateTs = new Date(fbTs.toMillis());
-	return `${dateTs?.toLocaleDateString()} ${dateTs?.toLocaleTimeString()}`;
-};
