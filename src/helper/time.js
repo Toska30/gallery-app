052872a455d6492d4604de15f2465091ead45994
Blerta Toska <36380101+Toska30@users.noreply.github.com>
2022-03-02 10:01:03 +0100

    Delete src/helper directory

--- src/helper/time.js
+++
@@ -1,5 +0,0 @@
-export const createDateTimeString = timestamp => {
-    if (!timestamp) return null
-    const dateTime = new Date(timestamp.toMillis())
-    return `${dateTime.toLocaleDateString()} ${dateTime.toLocaleTimeString()}`
-  }
